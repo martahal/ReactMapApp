6e5d1dfb8e3dbf910f74f5a71ed590d31eee84f1
martahal <martahal@stud.ntnu.no>
2020-10-21 17:19:20 +0200

    second commit

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -19,7 +19,7 @@ const MapboxGLMap = () => {
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
         center: [10.408773,63.422091],
-        zoom: 10
+        zoom: 5
       });
 
       map.on("load", () => {
